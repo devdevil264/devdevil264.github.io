d36ab589dfdc4feeda6a024e8d869f0571571477
devdevil264 <101852200+devdevil264@users.noreply.github.com>
2026-05-12 20:57:12 -0400

    Add files via upload

---
+++ chapter12/lab2/rescue-bootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Devonte Salmon
+    File Name: script.js
+    Date: May 1, 2026
+*/
+
+// jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
