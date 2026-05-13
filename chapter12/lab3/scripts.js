dfb2d5847526148386377aaaa125b9d56a49ae30
devdevil264 <101852200+devdevil264@users.noreply.github.com>
2026-05-12 20:57:41 -0400

    Add files via upload

---
+++ chapter12/lab3/scripts.js
@@ -0,0 +1,34 @@
+/*
+Author: Devonte Salmon
+File Name: script.js
+Date: 5/11/2026
+*/
+
+// Hamburger menu function
+
+function hamburger() {
+
+    var menu = document.getElementById("menu-links");
+
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    }
+
+    else {
+        menu.style.display = "block";
+    }
+}
+
+/*
+Second function:
+Displays a welcome alert message
+*/
+function welcomeMessage() {
+    console.log("Welcome to Campus Events");
+}
+
+function welcomeMessage() {
+
+    alert("Welcome to Campus Events Hub!");
+
+}
